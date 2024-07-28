72d8258ff65ad48938d2fa7280783e5f640a160e
Aurelian-Nkonde <aurelianrobot@gmail.com>
2024-07-28 23:58:23 +0200

    back to basics

--- src/basics/core.clj
+++ src/basics/core.clj
@@ -1,122 +1,230 @@
 (ns basics.core
   (:gen-class))
 
-(defn greet-user [user]
-  (println (str "hello " user)))
-(defn greet
-  ([user] (println "hello " user))
-  ([user rank] (println "Sir " rank " how are you, " user))
-  )
-(defn greet-arity
-  ([name] (greet-arity name "Hello sir,"))
-  ([name message] (println message name))
-  )
-(defn number-of-args [& args] (println args))
-(defn all-args [& args] (println args))
+; It employs a uniform syntax!
+; Is written in a uniform structure, 2 kinds of structures
+; literal representantion [numbers, strings, vectors] and Operations 
+; literal + operators => Operations (code that does something!)
+; Clojure evaluates every code/form to produce a value!
 
-(defn multi-arity
-  ([name] (multi-arity name 0))
-  ([name age] (multi-arity name age "none@gmail.com"))
-  ([name age email] (println (str "Name: " name " Age: " age " Email: " email)))
-  )
+; Operations[How you do things] take the form (operator operand operand) 
+(+ 10 100)
+(str "hello" "world")
 
-(defn dispatch-book-format [book]
-  (cond
-    (vector? book) :vector-book
-    (contains? book :title) :standard-map
-    (contains? book :book) :alternative-map
-    ))
+; CONTROL FLOW!
+; basic flow operators if, do, when
+(if true
+	"this is true"
+	"this is false"
+)
 
-(defn my-first
-  [[first-thing]]
-  first-thing
-  )
-(defn hello-first [[thing]] thing)
-(defn choosing
-  [[first-choice & many-more]]
-  (println (str "first is => " first-choice))
-  (println (str "Many more.. " many-more))
-  )
+(if false
+	"this will not print out!"
+	"this will print out!"
+)
 
-(defn destructure-vector [[name]] name)
-(defn destructure-map [{hei :hei}] hei)
-;;breaking keywords of out a map
-(defn get-keywords
-  [{:keys [height width]}]
-  height width
-  )
+(if false
+	"this will not print" ; clojures returns nil
+)
 
-(def x-func (fn [x] (* x 2)))
-;;anonymous functions
-(map (fn [name] (println "Hello " name)) ["darth vader" "mr camera"])
-(defn greet-heroes [heroes] (map #(str "Hello, " %) heroes))
+; each branch can have one form/operation/code
+; to get around this, we use DO! [do many things!]
+; (if true 
+; 	(do (println "hello world")
+; 					(println "this is true")
+; 					true)
+; 	(do (println "this is false")
+; 					false
+; 	)
+; )
 
-(def no-name-1 (#(str "hello " %1) "john"))
-(def no-name-2 ((fn [x] (str "hello " x)) "thomas"))
-(defn destructure-vector-1 [[first second & rest]]
-  (println "first: " first)
-  (println "second: " second)
-  (println "rest: " rest)
-  )
+; WHEN is like a combination of IF and DO but with no else branch
+; (when true
+; 		(println "hello")
+; 		(println 100)
+; 		"this is true"
+; )
+
+; nil means no value
+(nil? 100)
+(nil? nil)
+
+; clojure equality operator is =
+(= 100 200)
+(if (= 100 100)
+	"the numbers are euqal!"
+	"the numbers are not equal!"
+)
 
 
-(def heroes-list ["john" "jack" "patrik" "tom"])
-(def top-h (let [top-heroes (take 2 heroes-list)] top-heroes))
-(defn -main
-  []
-  (println top-h))
+; Clojure has boolean operators  [or & and]
+; or, returns first truthy value or last value
+; and, returns first falsey value or last true value
+; (println (or 100 false nil "hello")) ;100
+; (println (or nil false nil 20)) ;20
+; (println (or nil nil false)) ;false
+
+; (println (and 100 "true" false "hello")) ;false
+; (println (and nil false "hello")) ;nil
+; (println (and 100 200 "hi")) ;hi
+
+; NAMING VALUES WITH DEF
+; use def to bind a name to a value!
+(def mein-name "thousand")
+(def best-cars ["bmw" "vw"])
+
+; DATA STRUCTURES
+; all data structures are immutable[can't change them in place]
+; NUMBERS 100 {int} 20.50 {float} 1/3 {ratio}
+; Only allow double quotes for strings
+(str "ola " "mundo")
+; str, concatenation
+
+; MAPS, similar to dictionaries or hashes
+; 2 kinds of maps in clojure, hash-maps[basic] and sorted maps
+; get, get-in
+
+(println {})
+(println {:name "thomas" :age 90})
+; can be nested
+(println {:name "thousnd" :address { :street "guns" :city "oslo" }})
+(def nurse-1 (hash-map :name "lucia" :age 23))
+; look up values with get
+(def nurse-2 {:name "chido" :height 300 :age 29 :adress {:street-name "fires" :city "london" :country "japan"}})
+(def nurse-2-name (get nurse-2 :name))
+; give get a default value
+(def nurse-2-age (get nurse-2 :ages "no age!"))
+; get-in, lets you look inside nested maps
+(def nurse-2-street-name (get-in nurse-2 [:adress :street-name]))
+; another way of looking up a value
+(def nurse-2-height (nurse-2 :height))
+
+; KEYWORDS, :
+; primarily used as keys in maps
+(println :first-keyword)
+; can be used as function that look up corresponding value in data structure
+(println (:color {:name "bmw" :color "red"})) ;;many do this
+
+; VECTORS
+; similar to an array, 0-indexed collection
+(println [100 400 false])
+; getting value at 3thd position
+(println (get [100 true "hi"] 2))
+(println (get ["something" "soweto" 100 false] 0))
+; elements can be of any type
+; create vector with function 
+(def vector-names (vector "this" "that" "here"))
+(def vector-names-is-vector (vector? vector-names))
+; conj, adding elements to the vector
+; (conj [1 2] 3)
+(def adding-with-conj (conj [1 2 3] 4))
+ 
+; LIST, similar to vectors but not entirely
+; diff, can't retrieve elements with get
+; '(1 2 3)
+(def list-of-names '("john" "mike" "peter"))
+; nth function to retrieve an element
+(nth '(1 3 4) 0)
+(nth '("hello" false 900) 2)
+; create using function
+(def create-list (list "first" true 35))
+; elements can be added at the beginning
+(def add-to-list (conj '("hello" 100) "new string"))
 
 
-;;Vector => Ordered collection of items
-;;[100 300 true] or vector 100 300 false
-;;count, first, last, rest, nth, conj[back], cons[front]
-;;conj names "new names", cons "new name" names
+; SETS
+; collections of unique values
+; clojure has 2 kind of sets, hash sets & sorted sets
+; creating hash set
+(def new-set #{"hello" 100})
+(def new-set-1 (hash-set 100 300 true))
+; multiple instances of a value become one unique value
+(def add-to-set (conj #{100 true} "hello"))
+; create sets from existing lists and vectors by set function
+(def set-from-a-list (set '(100 true 300 100)))
+(def set-from-a-vector (set ["hello" "hi" "ola" "hello"]))
+; check for membership using contains? function, or get , or keyword as function
+(println (contains? #{100 true} true))
+(println (:name #{:age :height :name}))
+(println (get #{"ops" "naine" "no"} "no"))
 
+; FUNCTIONS
+(println ((or + -) 1 4 5))
+(println ((and (= 1 1) + ) 1 2 3))
+(println ((first [+ 0]) 1 2 3))
+; functions that take other functions as arguments or returns a function are called [higher order functions]
 
-;;List => Ordered collection of items
-;; '(100 450 900) or list
+; function calls => expressions that have a function expression as the operator
+; 2 other kinds of expressions are macro calls and special forms
 
-;;Map => Key valued pairs
-;;{"name" "james" "age" 900} or (hash-map "name" "same" "email" "same@g.com")
-;;get, assoc(addss), dissoc(removes), keys, vals
+; special forms don't always evaluate all of their operands
+; also, you cant use them as function arguments
 
-;;Sets
-;;#{:name :lastName}
-;;contains; conj, disj
+(defn sample-function 
+	"this is a docstring"
+	[paramater-1]
+	(println (str "this is the body!, " paramater-1))
+)
+; DOC-STRING
+; is useful way to describe and document your code
+; PARAMETER AND ARITY
+; function arity => the number of function parameter
+; ARITY OVERLOADING => a function with different function bodies that run according to the number of arity[parameters]
+(defn multi-arity-1
+	([x]
+		(str "1 arity function"))
+	([x y]
+		(str "2 arity function"))
+	([x y z]
+	(str "3 arity function"))
+)
+;arity overloading is one way to provide default values for args 
+(defn best-languages
+	"this function prints out the best 2 languages"
+	([one two]
+	(str "Best languages are " one " and " two))
+	([one]
+	(best-languages one "clojure"))
+)
+; clojure also allows you to define a variable-arity functions by, rest parameter[put the rest in here as list]
+; indicated by an ampersand(&)
+(defn function-with-rest-parameter-1
+	[x & rest]
+	(println rest))
+; you can mix rest parameter with normal parameters but the rest has to come last
 
+; DESTRUCTERING 
+; the basic idea behind it that it lets you consisly bind names to values within a collection
+(defn first-destructured-function
+		[[first-item]]
+		(println first-item)
+)
+(defn first-destructured-function-2 
+	[[first second third & rest]]
+	(println (str "first: " first))
+	(println (str "second: " second))
+	(println (str "third: " third))
+	(println (str "rest: " rest))
+)
+(defn destructured-map
+	[{x :one y :two}]
+	(println (str "one: " x))
+	(println (str "two: " y))
+)
+; we often want to break keywords out of a map, here's the shorter syntax
+(defn destructured-map-short-syntax
+	[{:keys [height width]}]
+	(println (str "height: " height))
+	(println (str "width: " width))
+)
+; can retain access to the original map by using :as
+(defn destructured-map-short-syntax-retain-origin-map
+	[{:keys [weight height] :as original-map}]
+	(println (str "height: " height))
+	(println (str "weight: " weight))
+	(println (str "original map: " original-map))
+)
 
-;;if, = [check if things are equal] ,not= [check if 2 things are not equal]
-
-;;number? string? keyword? map? vector?
-
-;;cond, case
-
-;;Arity => a number of arguments a function takes
-;;multi arity functions, 3/4 normal number
-;;(defn greet
-;;  ([name, age](println "hello " name age))
-;;  ([name] (println "hello " name))
-;; )
-;; The idea of this filling in the defaults technique is that you have one
-;; arity, usually the one with the most arguments—that really does something. All the
-;; other arities, the ones that take fewer arguments, call that main version, filling
-;; in the missing parameters as they go
-
-;;any number of argument (defn all [& args] (println args))
-;;Varargs Or Variadic functions (defn all [x & args])
-
-;;Multimethods let you have a single function with many implementations
-;;picks the implementation based on any[like any] characteristic of its arguments
-;;(defmulti multimethodFunc)
-;;implementations for possible returned values (defmethod
-
-;;anonymous functions
-;; (fn [param-list] (function body))
-;; (#(* % 2) 10)
-;;function call => (* 1 2)
-;;anonymous function => #(* % 2), & is the argument passed
-;;many arguments %1, %2, %3 #(str %1 "has" %2 "cars")
-
-;;Functions can return other func, The returned functions are called "closures"
-
-;;let [let something be, binds names to values]
+(defn -main 
+	[]
+	(destructured-map-short-syntax-retain-origin-map {:height "200m" :weight "300m"}))
