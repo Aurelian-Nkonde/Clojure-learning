dc343d16c1652b31c6b00042184b9ea970969d7f
Aurelian-Nkonde <aurelianrobot@gmail.com>
2024-07-31 17:41:50 +0200

    more errors

--- src/basics/core.clj
+++ src/basics/core.clj
@@ -14,18 +14,16 @@
 ; CONTROL FLOW!
 ; basic flow operators if, do, when
 (if true
-	"this is true"
-	"this is false"
-)
+  "this is true"
+  "this is false")
 
 (if false
-	"this will not print out!"
-	"this will print out!"
-)
+  "this will not print out!"
+  "this will print out!")
 
 (if false
-	"this will not print" ; clojures returns nil
-)
+  "this will not print" ; clojures returns nil
+  )
 
 ; each branch can have one form/operation/code
 ; to get around this, we use DO! [do many things!]
@@ -52,9 +50,8 @@
 ; clojure equality operator is =
 (= 100 200)
 (if (= 100 100)
-	"the numbers are euqal!"
-	"the numbers are not equal!"
-)
+  "the numbers are euqal!"
+  "the numbers are not equal!")
 
 
 ; Clojure has boolean operators  [or & and]
@@ -87,7 +84,7 @@
 (println {})
 (println {:name "thomas" :age 90})
 ; can be nested
-(println {:name "thousnd" :address { :street "guns" :city "oslo" }})
+(println {:name "thousnd" :address {:street "guns" :city "oslo"}})
 (def nurse-1 (hash-map :name "lucia" :age 23))
 ; look up values with get
 (def nurse-2 {:name "chido" :height 300 :age 29 :adress {:street-name "fires" :city "london" :country "japan"}})
@@ -118,7 +115,7 @@
 ; conj, adding elements to the vector
 ; (conj [1 2] 3)
 (def adding-with-conj (conj [1 2 3] 4))
- 
+
 ; LIST, similar to vectors but not entirely
 ; diff, can't retrieve elements with get
 ; '(1 2 3)
@@ -150,7 +147,7 @@
 
 ; FUNCTIONS
 (println ((or + -) 1 4 5))
-(println ((and (= 1 1) + ) 1 2 3))
+(println ((and (= 1 1) +) 1 2 3))
 (println ((first [+ 0]) 1 2 3))
 ; functions that take other functions as arguments or returns a function are called [higher order functions]
 
@@ -160,71 +157,172 @@
 ; special forms don't always evaluate all of their operands
 ; also, you cant use them as function arguments
 
-(defn sample-function 
-	"this is a docstring"
-	[paramater-1]
-	(println (str "this is the body!, " paramater-1))
-)
+(defn sample-function
+  "this is a docstring"
+  [paramater-1]
+  (println (str "this is the body!, " paramater-1)))
 ; DOC-STRING
 ; is useful way to describe and document your code
 ; PARAMETER AND ARITY
 ; function arity => the number of function parameter
 ; ARITY OVERLOADING => a function with different function bodies that run according to the number of arity[parameters]
 (defn multi-arity-1
-	([x]
-		(str "1 arity function"))
-	([x y]
-		(str "2 arity function"))
-	([x y z]
-	(str "3 arity function"))
-)
+  ([x]
+   (str "1 arity function"))
+  ([x y]
+   (str "2 arity function"))
+  ([x y z]
+   (str "3 arity function")))
 ;arity overloading is one way to provide default values for args 
 (defn best-languages
-	"this function prints out the best 2 languages"
-	([one two]
-	(str "Best languages are " one " and " two))
-	([one]
-	(best-languages one "clojure"))
-)
+  "this function prints out the best 2 languages"
+  ([one two]
+   (str "Best languages are " one " and " two))
+  ([one]
+   (best-languages one "clojure")))
 ; clojure also allows you to define a variable-arity functions by, rest parameter[put the rest in here as list]
 ; indicated by an ampersand(&)
 (defn function-with-rest-parameter-1
-	[x & rest]
-	(println rest))
+  [x & rest]
+  (println rest))
 ; you can mix rest parameter with normal parameters but the rest has to come last
 
 ; DESTRUCTERING 
 ; the basic idea behind it that it lets you consisly bind names to values within a collection
 (defn first-destructured-function
-		[[first-item]]
-		(println first-item)
-)
-(defn first-destructured-function-2 
-	[[first second third & rest]]
-	(println (str "first: " first))
-	(println (str "second: " second))
-	(println (str "third: " third))
-	(println (str "rest: " rest))
-)
+  [[first-item]]
+  (println first-item))
+
+(defn first-destructured-function-2
+  [[first second third & rest]]
+  (println (str "first: " first))
+  (println (str "second: " second))
+  (println (str "third: " third))
+  (println (str "rest: " rest)))
+
 (defn destructured-map
-	[{x :one y :two}]
-	(println (str "one: " x))
-	(println (str "two: " y))
-)
+  [{x :one y :two}]
+  (println (str "one: " x))
+  (println (str "two: " y)))
 ; we often want to break keywords out of a map, here's the shorter syntax
+
 (defn destructured-map-short-syntax
-	[{:keys [height width]}]
-	(println (str "height: " height))
-	(println (str "width: " width))
-)
+  [{:keys [height width]}]
+  (println (str "height: " height))
+  (println (str "width: " width)))
 ; can retain access to the original map by using :as
+
 (defn destructured-map-short-syntax-retain-origin-map
-	[{:keys [weight height] :as original-map}]
-	(println (str "height: " height))
-	(println (str "weight: " weight))
-	(println (str "original map: " original-map))
+  [{:keys [weight height] :as original-map}]
+  (println (str "height: " height))
+  (println (str "weight: " weight))
+  (println (str "original map: " original-map)))
+
+; function body
+; clojure automatically returns the last value evaluated
+
+; anonymouse functions
+; you create anonymouse functions in 2 ways
+; fn form & #()
+(fn [x] (println "hello"))
+(def anonymouse-function-1 (fn [] (println "hello")))
+(def anonymouse-function-2 (fn [] (println (str "hello"))))
+(def anonymouse-function-3 (fn [x y] (println (+ y x))))
+(def anonymouse-function-4 (fn [x] (println (str "hello, " x))))
+
+; (#(str "hello " %)
+(def compact-way-1 (#(str "hello " %) "james"))
+(def compact-way-2 (#(+ %1 500) 100))
+
+; function can return other  functions
+; the returned functions are closures, [they can access all the variables that were in scope when the function was created]
+;  
+(defn inc-maker
+  [inc-numb]
+  #(+ % inc-numb))
+(def incc (inc-maker 10))
+
+
+; example app
+(def asym-hobbit-body-parts [{:name "left-head" :size 10}
+                             {:name "left-wings" :size 25}
+                             {:name "left-stomach" :size 70}
+                             {:name "left-legs" :size 50}
+                             {:name "left-hands" :size 12}])
+
+(defn matching-part
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-")
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  [asym-body-parts]
+  (loop [remaining-asym-parts asym-body-parts final-body-parts []]
+    (if (empty? remaining-asym-parts)
+      final-body-parts
+      (let [[part & remaining] remaining-asym-parts]
+        (recur remaining
+               (into final-body-parts
+                     (set [part (matching-part part)])))))))
+
+
+; let
+; let bind names to values!
+; think of it as [let it be]
+; (let [x 10] x)
+; let introduces a new scope
+(let [x 200] x)
+(let [x 200] 
+	(* x 2)
 )
 
-(defn -main 
-	[]
-	(destructured-map-short-syntax-retain-origin-map {:height "200m" :weight "300m"}))
+(def heroes-coders ["john" "mike" "tom" "patrik" "samson"])
+(def two-heroes (let [heroes (take 2 heroes-coders)] heroes))
+; you can also use rest parameters in let
+(def heroes-coders-1 ["anna" "mary" "sia" "jona"])
+(def first-and-rest-coders (let [[first & rest] heroes-coders-1] [first rest] ))
+
+; let form have 2 main uses
+; 1: they provide clarity by letting you name things
+; 2: they allow u to evaluate an expression only once and reuse the result
+
+; loop
+(loop [iteration 0]
+		(println (str "Number: " iteration))
+		(if (= iteration 5)
+			(println "Cool number, bye!")
+			(recur (inc iteration))
+		)
+)
+
+
+; regular expression
+; tools for perfoming pattern matching on text
+; #"regular-expression"
+; the carat ^, signals that it will match the text only if its at the beginning
+; you can test this with refind
+
+(def check-refind-1 (re-find #"^hello" "hello"))
+(def check-refind-2 (re-find #"^hello" "hello-world"))
+(def check-refind-3 (re-find #"^hello" "something"))
+(def check-refind-4 (re-find #"^what" "hello"))
+
+(defn matching-part-function
+	[part]
+	{:name (clojure.string/replace (:name part) #"large-" "small-")
+		:size (:size part)
+		})
+
+; reduce
+; a pattern of process each element in a sequence and build a result
+(reduce + [1 2 3])
+; reduce can also take an initial value
+(reduce + 10 [2 3])
+
+(def reduce-1 (reduce + [10 10 20]))
+(def reduce-2 (reduce + 10 [5 2]))
+
+
+(defn -main
+  []
+  (println reduce-2))
